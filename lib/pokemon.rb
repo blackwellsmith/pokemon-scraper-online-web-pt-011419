c0e94ae01742598304f9aca6bab0a10cc9fde8a9
blackwell <blackwellsmithiv@gmail.com>
2019-04-02 11:53:46 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,26 +1,19 @@
 require "pry"
 class Pokemon
-  attr_accessor :id ,:name, :type, :db
+  attr_accessor :id ,:name, :type, :hp, :db
   @@all = []
   
-  def initialize(id:,name:,type:, db:)
+  def initialize(id:,name:,type:,hp: nil, db:)
     @id = id
     @name = name 
     @type = type
     @db = db
+    @hp = hp
     @@all << self
   end
   def self.find(number, db)
-    pokemon_by_id = db.execute("SELECT * FROM pokemon WHERE id = ?", [number])
-#binding.pry
-    
-    id = pokemon_by_id[0][0]
-    name = pokemon_by_id[0][1]
-    type = pokemon_by_id[0][2]
-    new_pokemon = Pokemon.new(id:id,name:name,type:type,db:db)
-   #binding.pry
-    return new_pokemon
-   # binding.pry
+    pokemon_by_id = db.execute("SELECT * FROM pokemon WHERE id = ?", [number]).flatten
+   Pokemon.new(id: pokemon_by_id[0],name: pokemon_by_id[1],type: pokemon_by_id[2],db:db)
   end
   
   def self.save(name, type, db)
